de6dd34aead227831813db1613f062974a5ef6a8
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-14 10:32:22 +0000

    :construction: test to check quality of an item does reduce below 0 when updating item written

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -15,5 +15,11 @@ describe GildedRose do
       expect(items[0].sell_in).to eq 0
       expect(items[0].quality).to eq 50
     end
+
+    it "shoud not reduce the quality of an item below 0" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect { GildedRose.new(items).update_quality() }.to change { items[0].quality }.by 0
+    end
   end
 end
