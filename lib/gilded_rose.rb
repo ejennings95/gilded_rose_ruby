00c479055c8d8e8a3eae7c5454041b06cebb3fc7
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-14 15:23:56 +0000

    :recycle: refactor guard clause to check if quality decreases after each day into own method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,10 +6,10 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if decrease_quality_as_sell_in_approaches?(item)
         reduce_quality_by_1(item)
       else
-        item_quality_less_than_50?(item)
+        item_quality_less_than_50(item)
         under_10_sell_in_value(item)
       end
       reduce_sell_in_by_1(item)
@@ -19,7 +19,11 @@ class GildedRose
 
   private
 
-  def item_quality_less_than_50?(item)
+  def decrease_quality_as_sell_in_approaches?(item)
+    (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") ? true : false
+  end
+
+  def item_quality_less_than_50(item)
     item.quality += 1 if item.quality < 50
   end
 
@@ -30,8 +34,8 @@ class GildedRose
   def sell_in_less_than_0?(item)
     if item.sell_in < 0
       item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
-      item_quality_less_than_50?(item) if item.name == "Aged Brie"
-      reduce_quality_by_1(item) if item.name != "Aged Brie" 
+      item_quality_less_than_50(item) if item.name == "Aged Brie"
+      reduce_quality_by_1(item) if item.name != "Aged Brie"
     end
   end
 
@@ -40,8 +44,8 @@ class GildedRose
   end
 
   def under_10_sell_in_value(item)
-    item_quality_less_than_50?(item) if (item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 11)
-    item_quality_less_than_50?(item) if (item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 6)
+    item_quality_less_than_50(item) if (item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 11)
+    item_quality_less_than_50(item) if (item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sell_in < 6)
   end
 end
 
