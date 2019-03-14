58ffc9d1e8d3f0e777df6301ca1c6ef0ffd92aa5
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-14 10:25:24 +0000

    :construction: test to check sulfuras properties arent chnaged  when updating item written

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,5 +8,12 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "should not chnage the SellIn or Quality of Sulfuras" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+      expect(items[0].quality).to eq 50
+    end
   end
 end
