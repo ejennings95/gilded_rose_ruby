37787df8664890fa4c24a194f301e52f61d5f20b
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-03-14 10:42:15 +0000

    :white_check_mark: update tests to include context

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,20 +2,24 @@ require_relative '../lib/gilded_rose.rb'
 
 describe GildedRose do
 
-  context "#update_quality" do
+  context "#Item_name" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+  end
 
+  context "#Sulfuras" do
     it "should not chnage the SellIn or Quality of Sulfuras" do
       items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq 0
       expect(items[0].quality).to eq 50
     end
+  end
 
+  context "#Quality_of_item_0" do
     it "shoud not reduce the quality of an item below 0" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
